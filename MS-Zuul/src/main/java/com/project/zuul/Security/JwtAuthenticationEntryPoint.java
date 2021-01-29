d1ff5f2386eae76f1dffbc1e29c268106dcdae7c
brullyz <brullyz01@gmail.com>
2021-01-29 07:27:28 +0700

    zuul service

---
+++ MS-Zuul/src/main/java/com/project/zuul/Security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.project.zuul.Security;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7663541641846063553L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException e) throws IOException, ServletException {
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+		
+		String json = String.format("{\"message\": \"%s\"}", e.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);  
+	}
+
+}
